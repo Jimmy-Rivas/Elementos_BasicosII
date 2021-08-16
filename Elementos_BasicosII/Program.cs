0a617186e1fae847eb9f56fd1c7fbaca34501d5d
Jimmy Rivas <jimmyrivas568@gmail>
2021-08-15 22:14:15 -0600

    ejemplo loops

--- Elementos_BasicosII/Program.cs
+++ Elementos_BasicosII/Program.cs
@@ -11,12 +11,13 @@ namespace Elementos_BasicosII
         static void Main(string[] args)
         {
             //loops
-
+            loop(); // llamado el metodo loop
+            Console.ReadKey();
         }
         // Whille
         // evalua la condicion y ejecuta las instrucciones
         // si la condicion es verdadera 
-        public static void loopWhile()
+        public static void loop()
         {
             int[] nums = new int[3]; // crea arreglo 
 
@@ -26,11 +27,33 @@ namespace Elementos_BasicosII
             nums[2] = 8;
 
             // imprimir los numeros 
-            int i = 0;
+            int i = 0, idw = 0, ifor = 0;
+            Console.WriteLine("Imprimiendo con while");
             while (i < nums.Length )
+
             {
-                Console.WriteLine(nums[i]);
+               
+                Console.WriteLine("indice {0}: {1}", i,nums[i]);
                 i++;
+            }
+            //Imprimir con ciclo dowhile
+            //la condicion se valua al final
+            Console.WriteLine("imprimiendo con Do-while: ");
+            do 
+            {
+
+          
+                Console.WriteLine("indice {0}: {1}", idw,nums[idw]);
+                idw++;
+
+            } while (idw < nums.Length);
+            //Imprimir con ciclo for 
+            // se comprueba la condicion primero y ejecuta si la condicion es verdadera 
+            Console.WriteLine("imprimiendo con for");
+            for (int inc = 0; inc < nums.Length; inc++) 
+            {
+                Console.WriteLine("indice {0}: {1} ", inc , nums[inc]);
+
             }
         }
     }
